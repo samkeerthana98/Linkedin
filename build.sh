63c307d54e9c05734072bcd7b61fe32241c91c80
samkeerthana98 <157691995+samkeerthana98@users.noreply.github.com>
2025-08-04 13:57:59 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,12 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Change to backend directory for Django commands
+cd backend
+
 # Collect static files
-python project/backend/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Run migrations
-python project/backend/manage.py migrate
+python manage.py migrate
 
