e73beb1558167be734d0c6ca61b92a008b9a19d5
samkeerthana98 <157691995+samkeerthana98@users.noreply.github.com>
2025-08-04 20:13:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
+
+# Install dependencies
 pip install -r requirements.txt
-cd project/backend
+
+# Collect static files
 python project/backend/manage.py collectstatic --no-input
+
+# Apply migrations
 python project/backend/manage.py migrate
